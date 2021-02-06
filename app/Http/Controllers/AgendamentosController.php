4c8dc78ac8eb7957c3b693d5a541baf24567c8b4
Patrick Dupin <patrickfdupin@gmail.com>
2021-02-06 15:42:57 -0300

    desenvolvimento front

--- app/Http/Controllers/AgendamentosController.php
+++ app/Http/Controllers/AgendamentosController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cliente;
 
 class AgendamentosController extends Controller {
     public function index () {
-        return view('agendamentos.index');
+        $clientes = Cliente::query()
+                ->orderBy('nome')
+                ->get();
+
+        return view('agendamentos.index', compact('clientes'));
+    }
+
+    public function agendar (Request $request) {
+        var_dump($request);exit;
     }
 }
--- resources/views/agendamentos/index.blade.php
+++ resources/views/agendamentos/index.blade.php
@@ -1,9 +1,38 @@
 @extends('templates.layout')
 
 @section('conteudo')
-<a href="{{ route('agendarCliente') }}" class="btn btn-info mt-3 mb-3"><i class="fas fa-calendar">&nbsp;</i> Agendar</a>
+<h1>Agendamento</h1>
+<form action="" method="post">@csrf
+    <div class="row pt-4">
+        <div class="col">
+            <label for="cliente">Cliente</label>
+            <select name="cliente" id="cliente" class="form-control">
+                <option value="" disabled="disabled" selected="selected">Selecione um cliente</option>
+                @foreach ($clientes as $cliente)
+                <option value="{{ $cliente->id }}">{{ $cliente->nome }}</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col">
+            <label for="profissional">Profissional</label>
+            <select name="profissional" id="profissional" class="form-control">
+                <option value="" disabled="disabled" selected="selected">Selecione um profissional</option>
+            </select>
+        </div>
+        <div class="col">
+            <label for="servico">Serviço</label>
+            <select name="servico" id="servico" class="form-control">
+                <option value="" disabled="disabled" selected="selected">Selecione um serviço</option>
+            </select>
+        </div>
+    </div>
 
-<div>
-    <p>Agendamento</p>
-</div>
+    <div class="row">
+        <div class="col">
+        <a href="{{ route('agendarCliente') }}" class="btn btn-info mt-3 mb-3"><i class="fas fa-calendar">&nbsp;</i> Agendar</a>
+        </div>
+    </div>
+</form>
 @endsection('conteudo')
